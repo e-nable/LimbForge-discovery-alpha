a82ef3bba92d2642171624ce0505ab4961f09876
Kaitlyn Hova <kaitlynhova@gmail.com>
2016-07-15 15:17:50 -0700

    adding C4 & L1 input functionality
    - forcing decimal format
    - translating decimal to script readable format
    - error handling for out of bounds input values
    - line spacing
    - changing default hand to “left”

--- js/src/handLoader.js
+++ js/src/handLoader.js
@@ -1,145 +1,144 @@
 var HandLoader = function(manifest,design){
-    if (!manifest || !manifest.parts) throw new Error("Expected a manifest with a parts array.");
+  if (!manifest || !manifest.parts) throw new Error("Expected a manifest with a parts array.");
 
-    var currentMesh, originalMesh, flippedMesh,
-        flipped = false;
+  var currentMesh, originalMesh, flippedMesh,
+  flipped = false;
 
-    function getParts(hand,size,design){
+  function getParts(hand,l1,c4,design){
+    if (hand !== "left" && hand !== "right") throw new Error("Expected hand to be either right or left");
+    if (typeof l1 != "number" || l1 < 0 || l1 > 500) throw new Error("Expected L1 size to be a number between 22cm - 30cm");
+    if (typeof c4 != "number" || c4 < 0 || c4 > 500) throw new Error("Expected C4 size to be a number between 20cm - 30cm");
+    if (typeof design != "object") throw new Error("Expected design to be an object");
 
+    return _.filter(manifest["parts"],function(part){
+      return part["handedness"].indexOf(hand) > -1; // if "handedness" includes the specified handedness, it's included!
+    });
+  }
 
-        if (hand !== "left" && hand !== "right") throw new Error("Expected hand to be either right or left");
-        if (typeof size != "number" || size < 100 || size > 200) throw new Error("Expected size to be a number between 100 and 200");
-        if (typeof design != "object") throw new Error("Expected design to be an object");
+  function loadDisplayModel(hand,size,design,cb){
+    // This function assumes there's one display model which is scaled and mirrored for the user's benefit.
+    // This display model is NOT THE SAME as the files that are downloaded for printing.
+    var loader = new THREE.STLLoader();
+    loader.load( design.directory + manifest["displayModel"], function ( geometry, data ) {
 
-        return _.filter(manifest["parts"],function(part){
-            return part["handedness"].indexOf(hand) > -1; // if "handedness" includes the specified handedness, it's included!
-        });
+      var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
+      material.side = THREE.DoubleSide;
+      var mesh = new THREE.Mesh( geometry, material );
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+
+      // Manipulate the mesh according to display pref's, specifically "hand" (if it needs to be mirrored) and
+      // "size" to zoom it with reference to a fixed grid
+      mesh.rotation.set(-Math.PI*0.45,0,0 );
+      mesh.position.set(0,0,0);
+
+      scene.add( mesh );
+
+      // Store references so the display model can be manipulated
+      originalMesh = currentMesh = mesh;
+      flippedMesh = generateFlippedMeshFromMesh(mesh);
+
+      if (cb) cb(data);
+    } );
+  }
+
+  function generateFlippedMeshFromMesh(mesh){
+    // create separate mesh for flipped view. Lighting on flipped faces looks super weird.
+    // See http://stackoverflow.com/questions/27722441/loading-object-as-geometry-instead-of-buffergeometry-in-threejs
+    var flippedGeometry = new THREE.Geometry().fromBufferGeometry( mesh.geometry );
+    flippedGeometry.dynamic = true;
+
+    for (var i = 0, l = flippedGeometry.faces.length; i < l; i++) {
+
+      var face = flippedGeometry.faces[i];
+      var temp = face.a;
+      face.a = face.c;
+      face.c = temp;
+
     }
 
-    function loadDisplayModel(hand,size,design,cb){
-        // This function assumes there's one display model which is scaled and mirrored for the user's benefit.
-        // This display model is NOT THE SAME as the files that are downloaded for printing.
-        var loader = new THREE.STLLoader();
-        loader.load( design.directory + manifest["displayModel"], function ( geometry, data ) {
+    var faceVertexUvs = flippedGeometry.faceVertexUvs[0];
+    for (i = 0, l = faceVertexUvs.length; i < l; i++) {
 
-            var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
-            material.side = THREE.DoubleSide;
-            var mesh = new THREE.Mesh( geometry, material );
-            mesh.castShadow = true;
-            mesh.receiveShadow = true;
-
-            // Manipulate the mesh according to display pref's, specifically "hand" (if it needs to be mirrored) and
-            // "size" to zoom it with reference to a fixed grid
-            mesh.rotation.set(-Math.PI*0.45,0,0 );
-            mesh.position.set(0,0,0);
-
-            scene.add( mesh );
-
-            // Store references so the display model can be manipulated
-            originalMesh = currentMesh = mesh;
-            flippedMesh = generateFlippedMeshFromMesh(mesh);
-
-            if (cb) cb(data);
-        } );
+      var vector2 = faceVertexUvs[i][0];
+      faceVertexUvs[i][0] = faceVertexUvs[i][2];
+      faceVertexUvs[i][2] = vector2;
     }
 
-    function generateFlippedMeshFromMesh(mesh){
-        // create separate mesh for flipped view. Lighting on flipped faces looks super weird.
-        // See http://stackoverflow.com/questions/27722441/loading-object-as-geometry-instead-of-buffergeometry-in-threejs
-        var flippedGeometry = new THREE.Geometry().fromBufferGeometry( mesh.geometry );
-        flippedGeometry.dynamic = true;
+    var flipped = new THREE.Mesh( flippedGeometry, mesh.material );
+    flipped.rotation.set(mesh.rotation.x,mesh.rotation.y,mesh.rotation.z );
+    flipped.scale.set(-1,1,1);
 
-        for (var i = 0, l = flippedGeometry.faces.length; i < l; i++) {
+    return flipped;
+  }
 
-            var face = flippedGeometry.faces[i];
-            var temp = face.a;
-            face.a = face.c;
-            face.c = temp;
+  return {
+    loadDisplayHand : function(hand,size,design,cb){
+      loadDisplayModel(hand,size,design,cb);
+    },
+    setDisplayModelSize: function(size) {
+      if (currentMesh) {
+        currentMesh.scale.set((flipped ? -1 : 1 ) * size/100,size/100,size/100);
+        render();
+      }
+    },
+    flip: function(flip){
+      flipped = flip;
 
-        }
+      var currScale = Math.abs(currentMesh.scale.y);
 
-        var faceVertexUvs = flippedGeometry.faceVertexUvs[0];
-        for (i = 0, l = faceVertexUvs.length; i < l; i++) {
+      flippedMesh.scale.set(-1*currScale,currScale,currScale);
+      originalMesh.scale.set(currScale,currScale,currScale);
 
-            var vector2 = faceVertexUvs[i][0];
-            faceVertexUvs[i][0] = faceVertexUvs[i][2];
-            faceVertexUvs[i][2] = vector2;
-        }
+      currentMesh = flipped ? flippedMesh : originalMesh;
+      var otherMesh = flipped ? originalMesh : flippedMesh;
 
-        var flipped = new THREE.Mesh( flippedGeometry, mesh.material );
-        flipped.rotation.set(mesh.rotation.x,mesh.rotation.y,mesh.rotation.z );
-        flipped.scale.set(-1,1,1);
+      scene.remove(otherMesh);
+      scene.add(currentMesh);
 
-        return flipped;
-    }
+      render();
+    },
+    getFiles: function(hand,l1,c4,design,successCallback,errorCallback){
+      var parts = getParts(hand,l1,c4,design);
+      var files = [];
+      var completedDownloads = 0;
 
-    return {
-        loadDisplayHand : function(hand,size,design,cb){
-            loadDisplayModel(hand,size,design,cb);
-        },
-        setDisplayModelSize: function(size) {
-            if (currentMesh) {
-                currentMesh.scale.set((flipped ? -1 : 1 ) * size/100,size/100,size/100);
-                render();
+      // Trigger a download of each part via xhr, specifying blob type
+      _.each(parts,function(p){
+
+        (function(){
+          var partfilename = _.template(p.file_name_template)({handedness:hand,l1:l1,c4:c4});
+          var partDirname = design.directory + _.template(p.file_dir_template)({handedness:hand,l1:l1,c4:c4});
+
+          // Wrap xhr in anonymous function to guarantee xhr variable has its own scope for each download
+          var xhr = new XMLHttpRequest();
+          xhr.addEventListener('load', function(){
+
+            if (xhr.status == 200){
+              //Do something with xhr.response (not responseText), which should be a Blob
+              files.push({
+                name: partfilename,
+                blob: xhr.response
+                //                            blob: new Blob([arrayBuffers[elementName]], {type: "application/sla"})
+              });
+            } else {
+              // Error encountered!
+              errorCallback(p.name);
             }
-        },
-        flip: function(flip){
-            flipped = flip;
 
-            var currScale = Math.abs(currentMesh.scale.y);
+            completedDownloads += 1;
 
-            flippedMesh.scale.set(-1*currScale,currScale,currScale);
-            originalMesh.scale.set(currScale,currScale,currScale);
+            if (completedDownloads == parts.length) {
+              successCallback(files);
+            }
+          });
 
-            currentMesh = flipped ? flippedMesh : originalMesh;
-            var otherMesh = flipped ? originalMesh : flippedMesh;
-
-            scene.remove(otherMesh);
-            scene.add(currentMesh);
-
-            render();
-        },
-        getFiles: function(hand,size,design,successCallback,errorCallback){
-            var parts = getParts(hand,size,design);
-            var files = [];
-            var completedDownloads = 0;
-
-            // Trigger a download of each part via xhr, specifying blob type
-            _.each(parts,function(p){
-
-                (function(){
-                    var partfilename = _.template(p.file_name_template)({handedness:hand,size:size});
-                    var partDirname = design.directory + _.template(p.file_dir_template)({handedness:hand,size:size});
-
-                    // Wrap xhr in anonymous function to guarantee xhr variable has its own scope for each download
-                    var xhr = new XMLHttpRequest();
-                    xhr.addEventListener('load', function(){
-
-                        if (xhr.status == 200){
-                            //Do something with xhr.response (not responseText), which should be a Blob
-                            files.push({
-                                name: partfilename,
-                                blob: xhr.response
-                                //                            blob: new Blob([arrayBuffers[elementName]], {type: "application/sla"})
-                            });
-                        } else {
-                            // Error encountered!
-                            errorCallback(p.name);
-                        }
-
-                        completedDownloads += 1;
-
-                        if (completedDownloads == parts.length) {
-                            successCallback(files);
-                        }
-                    });
-
-                    xhr.open('GET', partDirname + partfilename);
-                    xhr.responseType = 'blob';
-                    xhr.send(null);
-                })();
-            });
-        }
-    };
+          xhr.open('GET', partDirname + partfilename);
+          xhr.responseType = 'blob';
+          xhr.send(null);
+        })();
+      });
+    }
+  };
 
 };
--- js/src/limbforge-ui.js
+++ js/src/limbforge-ui.js
@@ -1,160 +1,164 @@
 
 // Specify an initial hand
 var specs = {
-        hand: "right",
-        size: 100,
-        name: "matt",
-        l1: 100,
-        c4: 100,
-        design: {
-            name: "",
-            directory: ""
-        }
-    },
-    designs = [],
-    hl;
+  hand: "left",
+  size: 100,
+  name: "matt",
+  l1: 23,
+  c4: 23,
+  design: {
+    name: "",
+    directory: ""
+  }
+},
+designs = [],
+hl;
 
 $(document).ready(function(){
-    // Fetch the manifest so it's available everywhere!
-    $.get('designs.json').then(function(data){
-        // store reference to full list of designs so they can be shown in a drop down.
-        designs = data["designs"];
+  // Fetch the manifest so it's available everywhere!
+  $.get('designs.json').then(function(data){
+    // store reference to full list of designs so they can be shown in a drop down.
+    designs = data["designs"];
 
-        // Populate design dropdown
-        var $selector = $('#designSelector');
-        _.each(designs,function(des){
-            $selector.append("<option value='"+des.name+"'>"+des.name+"</option>");
-        });
-
-        // Listen for change events so the design can be selected
-        $selector.on('change',function(e){
-            var selected = _.findWhere(designs,{name: $(e.target).val()});
-            selectDesign(selected);
-        });
-
-        // Load up the first design by default (raptor reloaded as of this writing)
-        $selector.trigger("change");
+    // Populate design dropdown
+    var $selector = $('#designSelector');
+    _.each(designs,function(des){
+      $selector.append("<option value='"+des.name+"'>"+des.name+"</option>");
     });
 
-    function selectDesign(design){
-        // This will be passed to other functions for loading and display
-        specs.design = design;
-
-        return $.get(design["directory"] + "manifest.json")
-            .then(function(manifest){
-                //////////////// Go !
-                hl = new HandLoader(manifest,design);
-                reloadDisplayHand();
-            });
-    }
-
-    // Size Slider
-    $('#flat-slider').slider({
-        orientation: 'horizontal',
-        range:       false,
-        value:      specs.size,
-        min: 100,
-        max: 150,
-        step: 5,
-        slide: function( event, ui ) {
-            $( "#sizeFeedback" ).text( "Size: " + ui.value + "%" );
-            hl.setDisplayModelSize(ui.value);
-        },
-        stop: function( event, ui) {
-            specs.size = ui.value;
-        }
+    // Listen for change events so the design can be selected
+    $selector.on('change',function(e){
+      var selected = _.findWhere(designs,{name: $(e.target).val()});
+      selectDesign(selected);
     });
 
-    // Hand Selector
-    var $handSelectors = $('.hand-graphic');
-    $handSelectors.click(function(){
-        $handSelectors.removeClass("selected");
-        $(this).addClass("selected");
-        specs.hand = $(this).data("selectedhand");
-        hl.flip(specs.hand == "right");
+    // Load up the first design by default (raptor reloaded as of this writing)
+    $selector.trigger("change");
+  });
+
+  function selectDesign(design){
+    // This will be passed to other functions for loading and display
+    specs.design = design;
+
+    return $.get(design["directory"] + "manifest.json")
+    .then(function(manifest){
+      //////////////// Go !
+      hl = new HandLoader(manifest,design);
+      reloadDisplayHand();
+    });
+  }
+
+  // Size Slider
+  $('#flat-slider').slider({
+    orientation: 'horizontal',
+    range:       false,
+    value:      specs.size,
+    min: 100,
+    max: 150,
+    step: 5,
+    slide: function( event, ui ) {
+      $( "#sizeFeedback" ).text( "Size: " + ui.value + "%" );
+      hl.setDisplayModelSize(ui.value);
+    },
+    stop: function( event, ui) {
+      specs.size = ui.value;
+    }
+  });
+
+  // Hand Selector
+  var $handSelectors = $('.hand-graphic');
+  $handSelectors.click(function(){
+    $handSelectors.removeClass("selected");
+    $(this).addClass("selected");
+    specs.hand = $(this).data("selectedhand");
+    hl.flip(specs.hand == "right");
+  });
+
+  // Download Button
+  $('.downloadBtn').click(function(e){
+    e.preventDefault();
+    specs.name = $("#name").val();
+    specs.l1 = translateValue($("#L1").val());
+    specs.c4 = translateValue($("#C4").val());
+    downloadHand();
+  });
+
+  function translateValue(input){
+    var base_num = parseFloat(input.replace(".", ""));
+    return base_num
+  }
+
+  function zipFileName(specs){
+    var today = new Date();
+    return "LimbForge_" + specs.name.replace(/ /g,'') + "_" + specs.hand + "_L1-" + specs.l1 + "_C4-" + specs.c4 + "_" + today + ".zip";
+  }
+
+  function GAObjectForSpecs(specs){
+    // Create a 'productFieldObject' from the current hand specs
+    return {
+      'id': specs.design.name + specs.size + specs.hand,				// Product ID (string).
+      'name': specs.design.name + "_" + specs.size + "_" + specs.hand, // Product name (string).
+      'category': specs.design.name,   								// Product category (string).
+      'variant': specs.hand,            							// Product variant (string).
+      'position': specs.size           							// Product position (number).
+      //'dimension1': 'Member',            							// Custom dimension (string).
+      //'brand': 'Google',                							// Product brand (string).
+      //'list': 'Search Results',         							// Product list (string).
+    }
+  }
+
+  function reloadDisplayHand(){
+    // Remove any prior hand that was there
+    clearScene();
+    console.log(specs);
+    // Display some STLs
+    hl.loadDisplayHand(specs.hand,specs.size,specs.design,function(){
+      ga('ec:addProduct',GAObjectForSpecs(specs));
+      ga('ec:setAction', 'checkout',{
+        'step': 1
+      });
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'HandForge',
+        eventAction: 'preview',
+        eventLabel: window.location.host
+      });
+
+      // Defined in setup.js
+      render();
+
+      setTimeout(function(){render();},500);
+    });
+  }
+
+  function downloadHand(){
+    ga('ec:addProduct',GAObjectForSpecs(specs));
+    ga('ec:setAction', 'checkout_option',{
+      'step': 2
     });
 
-    // Download Button
-    $('.downloadBtn').click(function(e){
-        e.preventDefault();
-        specs.name = $("#name").val();
-        specs.l1 = parseInt($("#L1").val());
-        specs.c4 = parseInt($("#C4").val());
-        downloadHand();
+    //send params to get files
+    hl.getFiles(specs.hand,specs.l1, specs.c4,specs.design,function(files){
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'HandForge',
+        eventAction: 'download',
+        eventLabel: window.location.host
+      });
+
+      HandZipper.zip(zipFileName(specs),files);
+    },function(errors){
+      var message = "Some of the files you requested were not available:\n";
+      _.each(errors,function(e){
+        message += e;
+      });
+      alert(message);
+
+      ga('send', 'exception', {
+        'exDescription': "Incomplete_Hand_"+specs.hand + "_" + specs.size,
+        'exFatal': false
+      });
     });
-
-
-    function zipFileName(specs){
-      var today = new Date();
-        return "LimbForge_" + specs.name.replace(/ /g,'') + "_" + specs.hand + "_L1-" + specs.l1 + "_C4-" + specs.c4 + "_" + today + ".zip";
-    }
-
-    function GAObjectForSpecs(specs){
-        // Create a 'productFieldObject' from the current hand specs
-        return {
-            'id': specs.design.name + specs.size + specs.hand,				// Product ID (string).
-            'name': specs.design.name + "_" + specs.size + "_" + specs.hand, // Product name (string).
-            'category': specs.design.name,   								// Product category (string).
-            'variant': specs.hand,            							// Product variant (string).
-            'position': specs.size           							// Product position (number).
-            //'dimension1': 'Member',            							// Custom dimension (string).
-            //'brand': 'Google',                							// Product brand (string).
-            //'list': 'Search Results',         							// Product list (string).
-        }
-    }
-
-    function reloadDisplayHand(){
-        // Remove any prior hand that was there
-        clearScene();
-        console.log(specs);
-        // Display some STLs
-        hl.loadDisplayHand(specs.hand,specs.size,specs.design,function(){
-            ga('ec:addProduct',GAObjectForSpecs(specs));
-            ga('ec:setAction', 'checkout',{
-                'step': 1
-            });
-            ga('send', {
-                hitType: 'event',
-                eventCategory: 'HandForge',
-                eventAction: 'preview',
-                eventLabel: window.location.host
-            });
-
-            // Defined in setup.js
-            render();
-
-            setTimeout(function(){render();},500);
-        });
-    }
-
-    function downloadHand(){
-        ga('ec:addProduct',GAObjectForSpecs(specs));
-        ga('ec:setAction', 'checkout_option',{
-            'step': 2
-        });
-
-        //send params to get files
-        hl.getFiles(specs.hand,specs.size,specs.design,function(files){
-            ga('send', {
-                hitType: 'event',
-                eventCategory: 'HandForge',
-                eventAction: 'download',
-                eventLabel: window.location.host
-            });
-
-            HandZipper.zip(zipFileName(specs),files);
-        },function(errors){
-            var message = "Some of the files you requested were not available:\n";
-            _.each(errors,function(e){
-                message += "\n" +e;
-            });
-            alert(message);
-
-            ga('send', 'exception', {
-                'exDescription': "Incomplete_Hand_"+specs.hand + "_" + specs.size,
-                'exFatal': false
-            });
-        });
-    }
+  }
 
 });
