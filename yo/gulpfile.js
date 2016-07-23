1d51461254ce2cf22904de9eb6e7a587924de3f9
Mohammed A <mohammed.abuelhagag@gmail.com>
2016-07-23 22:33:51 +0200

    disable babel support
    breaks zip.js

--- yo/gulpfile.js
+++ yo/gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('scripts', () => {
   return gulp.src('app/scripts/**/*.js')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe($.babel())
+    //.pipe($.babel())
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({stream: true}));
